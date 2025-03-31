6c006edf759defbf753403834c7be1d12752ab20
Danylo Tymoshenko <dtymoshenko@griddynamics.com>
2025-03-31 09:44:36 +0200

    Initial commit

---
+++ Vikta-UI-testing-sample/src/test/java/com/griddynamics/qa/vikta/uitesting/sample/stepsDefinitions/LoginSteps.java
@@ -0,0 +1,69 @@
+package com.griddynamics.qa.vikta.uitesting.sample.stepsDefinitions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.shouldHaveThrown;
+
+import com.griddynamics.qa.vikta.uitesting.sample.auxiliary.DriverManager;
+import com.griddynamics.qa.vikta.uitesting.sample.config.DataProvider;
+import com.griddynamics.qa.vikta.uitesting.sample.pageObjects.LoginPage;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Login functionality related steps.
+ */
+public class LoginSteps extends BaseSteps {
+
+  private String baseUrl;
+
+  public LoginSteps(WebDriver driver) {
+    super(driver);
+  }
+
+  @Step
+  public void openLoginPage() {
+    System.out.println("NAVIGATION TO: " + getData().loginPageUrl());
+    getDriver().get(getData().loginPageUrl());
+  }
+
+  @Step
+  public void login(String username, String password) {
+    page().login(username, password);
+  }
+
+  @Step
+  public void loginAsRegularUser() {
+    page().login(getData().userName(), getData().userPassword());
+  }
+
+  @Step
+  public void loginAsAdmin() {
+    page().login(getData().adminName(), getData().adminPassword());
+  }
+
+  @Step
+  public void verifyCurrentPageIsHomePageForTheRegularUser() {
+    verifyCurrentPageIsHomePageForTheUser(getData().userName());
+  }
+
+  @Step
+  public void verifyCurrentPageIsHomePageForTheAdmin() {
+    verifyCurrentPageIsHomePageForTheUser(getData().adminName());
+  }
+
+  @Step
+  public void verifyErrorMessage(String text) {
+    getWait().until(ExpectedConditions.visibilityOf(page().getErrorWebElement()));
+    // Have a look at https://assertj.github.io/doc/
+    assertThat(page().getErrorMessage().trim())
+      .as("Error message was nor shown or had unexpected content.")
+      .contains(text);
+  }
+
+  //TODO: Think about generics etc instead of this.
+  private LoginPage page() {
+    return getPage(LoginPage.class);
+  }
+}
