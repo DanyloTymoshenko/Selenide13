634430b647339da1f72d264fd6a2fa377e4bc5f4
Danylo Tymoshenko <dtymoshenko@griddynamics.com>
2025-04-15 08:54:39 +0200

    Further xpath replacement. Login refactored to handle errors

--- Vikta-UI-testing-sample/src/main/java/com/griddynamics/qa/vikta/uitesting/sample/pageObjects/LoginPage.java
+++ Vikta-UI-testing-sample/src/main/java/com/griddynamics/qa/vikta/uitesting/sample/pageObjects/LoginPage.java
@@ -1,73 +1,95 @@
 package com.griddynamics.qa.vikta.uitesting.sample.pageObjects;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.time.Duration;
 
 /**
  * Page Object of Vikta's Login page
  */
 public class LoginPage {
 
-  //@FindBy(css = "#tfLoginname")
-  private SelenideElement txtLoginname() {
-    return $("#tfLoginname");
-  }
+    //@FindBy(css = "#tfLoginname")
+    private SelenideElement txtLoginname() {
+        return $("#tfLoginname");
+    }
 
-  //@FindBy(id = "tfPassword")
-  private SelenideElement txtPassword() {
-    return $("#tfPassword");
-  }
+    //@FindBy(id = "tfPassword")
+    private SelenideElement txtPassword() {
+        return $("#tfPassword");
+    }
 
-  // @FindBy(id = "btnSubmitLogin")
-  private SelenideElement btnSubmitLogin() {
-    return $("#btnSubmitLogin");
-  }
+    // @FindBy(id = "btnSubmitLogin")
+    private SelenideElement btnSubmitLogin() {
+        return $("#btnSubmitLogin");
+    }
 
-  //@FindBy(id = "btnSubmitGoToHome")
-  private SelenideElement btnGoToHome() {
-    return $("#btnSubmitGoToHome");
-  }
+    //@FindBy(id = "btnSubmitGoToHome")
+    private SelenideElement btnGoToHome() {
+        return $("#btnSubmitGoToHome");
+    }
 
-  //@FindBy(xpath = "//div[@class='login']//p[contains(@style, 'color: #FF1C19')]")
-  private SelenideElement lblError() {
-    return $("#body > div > div > div > div > form > div:nth-child(3) > p");
-  }
+    //@FindBy(xpath = "//div[@class='login']//p[contains(@style, 'color: #FF1C19')]")
+    private SelenideElement lblError() {
+        return $("body > div > div > div > div > form > div:nth-child(3) > p");
+    }
 
-  //TODO: Add [Sign-up] button.
-  @FindBy(id = "btnSubmitGoRegistration")
-  private WebElement btnSubmitGoRegistration;
+    //TODO: Add [Sign-up] button.
+    @FindBy(id = "btnSubmitGoRegistration")
+    private WebElement btnSubmitGoRegistration;
 
-  public void SubmitGoRegistration() {
-    btnSubmitGoRegistration.click();
-  }
+    public void SubmitGoRegistration() {
+        btnSubmitGoRegistration.click();
+    }
 
-  public HomePage login(String username, String password) {
-    tryLogin(username, password);
-    return new HomePage();
-  }
+    public HomePage login(String username, String password) {
+        tryLogin(username, password);
+        return new HomePage();
+    }
 
-  public void tryLogin(String username, String password) {
-    txtLoginname().sendKeys(username);
-    txtPassword().clear();
-    txtPassword().sendKeys(password);
 
-    btnSubmitLogin().click();
-  }
+    SelenideElement errMessage() {
+        return $("body > div > div > div > div > form > div:nth-child(3)");
+    }
 
-  public HomePage gotoHome() {
-    btnGoToHome().click();
+    public void tryLogin(String username, String password) {
+        try {
+            if (!WebDriverRunner.url().contains("/login")) {
+                Selenide.open("/login");
+            }
+            txtLoginname().shouldBe(visible, enabled).clear();
+            txtLoginname().setValue(username);
+            txtPassword().shouldBe(visible, enabled).clear();
+            txtPassword().setValue(password);
+            btnSubmitLogin().shouldBe(enabled).click();
+            if ($$("form p").stream().anyMatch(SelenideElement::isDisplayed)) {
+                System.out.println("Login failed: incorrect username or password.");
+            }
+        } catch (Exception e) {
+            System.err.println("Login attempt failed due to unexpected error: " + e.getMessage());
+        }
+    }
 
-    return new HomePage();
-  }
+    public HomePage gotoHome() {
+        btnGoToHome().click();
 
-  public String getErrorMessage() {
-    return lblError().getText();
-  }
+        return new HomePage();
+    }
 
-  public String getErrorWebElement() {
-    return lblError().getText();
-  }
+    public String getErrorMessage() {
+        return lblError().getText();
+    }
+
+    public String getErrorWebElement() {
+        return lblError().getText();
+    }
 }
